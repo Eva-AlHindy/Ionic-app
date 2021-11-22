7299d4421b32d967d613488427ae095d5fff3ea7
Eva-AlHindy <evahindy@gmail.com>
2021-11-22 14:38:49 +0100

    add componentDidLoad() {} to app-home

--- ionic-stencil-notepad/src/components/app-home/app-home.tsx
+++ ionic-stencil-notepad/src/components/app-home/app-home.tsx
@@ -5,23 +5,29 @@ import { Component, h } from '@stencil/core';
   styleUrl: 'app-home.css',
 })
 export class AppHome {
+  componentDidLoad() {}
+  addNote() {
+    const alertCtrl = document.querySelector('ion-alert-controller');
+    console.log(alertCtrl);
+  }
   render() {
     return [
       <ion-header>
         <ion-toolbar color="primary">
-          <ion-title>Home</ion-title>
+          <ion-title>Notes</ion-title>
+          <ion-buttons slot="end">
+            <ion-button>
+              <ion-icon slot="icon-only" name="clipboard" />
+            </ion-button>
+          </ion-buttons>
         </ion-toolbar>
       </ion-header>,
-
-      <ion-content class="ion-padding">
-        <p>
-          Welcome to the PWA Toolkit. You can use this starter to build entire apps with web components using Stencil and ionic/core! Check out the README for everything that comes
-          in this starter out of the box and check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
-        </p>
-
-        <ion-button href="/profile/ionic" expand="block">
-          Profile page
-        </ion-button>
+      <ion-content>
+        <ion-list>
+          <ion-item button detail>
+            <ion-label>Title goes here</ion-label>
+          </ion-item>
+        </ion-list>
       </ion-content>,
     ];
   }
